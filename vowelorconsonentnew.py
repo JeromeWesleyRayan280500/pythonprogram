b759dbd911899bce76d7b07891b38c02fdfa6318
JeromeWesleyRayan280500 <42993665+JeromeWesleyRayan280500@users.noreply.github.com>
2018-09-18 14:54:57 +0530

    Update vowelorconsonentnew.py

--- vowelorconsonentnew.py
+++ vowelorconsonentnew.py
@@ -1,7 +1,6 @@
-l=input("input a letter of the alphabetL:")
+l=input()
 if l in ('a','e','i','o','u'):
-print("%s is a vowel."%1)
-elif 1=="Y":
-print ("sometimes letter y stand for vowel,sometime stands for consotant.")
+    print("%s vowel."%1)
+
 else:
-print("%s is a consonent.:"%1)
+    print("%s consonent.:"%1)
