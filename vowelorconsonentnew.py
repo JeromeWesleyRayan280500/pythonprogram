9f51f66a3f03c33e02b9ce41277573379f438176
JeromeWesleyRayan280500 <42993665+JeromeWesleyRayan280500@users.noreply.github.com>
2018-09-18 14:46:22 +0530

    Rename vowelorconsonent.py to vowelorconsonentnew.py

---
+++ vowelorconsonentnew.py
@@ -0,0 +1,7 @@
+l=input("input a letter of the alphabetL:")
+if l in ('a','e','i','o','u'):
+print("%s is a vowel."%1)
+elif 1=="Y":
+print ("sometimes letter y stand for vowel,sometime stands for consotant.")
+else:
+print("%s is a consonent.:"%1)
